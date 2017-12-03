1b51621fc75812bae4b9e39dd0f89cc7ae1565db
DESKTOP-VDCJ6EF\PHAN NGOC YEN <yenpn.ali@gmail.com>
2017-12-03 15:00:30 +0700

    xong giao dien man hinh login giao dien man hinh chinh: 80%

---
+++ app/src/main/java/com/example/phanngocyen/cinemabox/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.phanngocyen.cinemabox;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.login_fb);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
